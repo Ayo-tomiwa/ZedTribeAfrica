dd932ab720bd96dec860e678d7cba1178c88e88d
Ayotomiwa Onifara <oyindml1@gmail.com>
2024-10-11 18:07:25 +0100

    Feat: completed the program section

--- src/Components/Programs/programs.jsx
+++ src/Components/Programs/programs.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './programs.css'
+import mentoring from '../../assets/mentoring.avif'
+import program_1 from '../../assets/program1.jpg'
+import program_2 from '../../assets/Program_2.jpg'
+import { SiCodementor } from "react-icons/si";
+import { SiFreecodecamp } from "react-icons/si";
+
 
 const Programs = () => {
   return (
     <div className='programs'>
-        <div className='program'>
-            <img src="" alt="" />
+        <div className='program program-mentor'>
+            <img src={mentoring} alt="" />
+            <div className="caption">
+              <span className='icon'>
+              <SiCodementor />
+              </span>
+                <p>Mentorships</p>
+            </div>
+        </div>
+        <div className='program program-image'>
+            <img src={program_1} alt="" />
+            <div className="caption">
+              <span className='icon'>
+              <SiFreecodecamp />
+              </span>
+                <p>Bootcamp Programs</p>
+            </div>
+        </div>
+        <div className='program program-image'>
+            <img src={program_2} alt="" />
+            <div className="caption">
+              <span className='icon'>
+              <SiFreecodecamp />
+              </span>
+                <p>Word For One</p>
+            </div>
         </div>
 
     </div>
