0b339a471206bb85a055538bcb07c6dfc364ddcc
Ayotomiwa Onifara <oyindml1@gmail.com>
2024-10-22 04:47:53 +0100

    feat: Completed responsive website

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import './navbar.css'
 import logo from '/src/assets/ZedTribeAfricaLogo__1c.png'
+import { Link } from 'react-scroll';
+import { RiMenu3Fill } from "react-icons/ri";
+
 
 const Navbar = () => {
 
@@ -10,19 +13,26 @@ const Navbar = () => {
       window.addEventListener('scroll', ()=>{
         window.scrollY > 50 ? setSticky(true) : setSticky(false);
       })
-    },[])
+    },[]);
+
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = ()=>{
+      mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
 
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <img src={logo} alt="zed-tribe-logo" className='logo' />
-        <ul>
-            <li className='hover'>Home</li>
-            <li className='hover'>Program</li>
-            <li className='hover'>About us</li>
-            <li className='hover'>Become a Sponsor</li>
-            <li className='hover'>Testimonials</li>
-            <li><button className='btn'>Contact us</button></li>
+        <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+            <li className='hover'><Link to='hero' smooth={true} offset={0}>Home</Link></li>
+            <li className='hover'><Link to='program' smooth={true} offset={-260}>Program</Link></li>
+            <li className='hover'><Link to='about' smooth={true} offset={-150}>About us</Link></li>
+            <li className='hover'><Link to='sponsor' smooth={true} offset={-260}>Become a Sponsor</Link></li>
+            <li className='hover'><Link to='testimonials' smooth={true} offset={-260}>Testimonials</Link></li>
+            <li><Link to='contact'  smooth={true} offset={-260} className='btn'>Contact us</Link></li>
         </ul>
+        <span className='menu-icon' onClick={toggleMenu}><RiMenu3Fill /></span>
     </nav>
   )
 }
--- src/Components/Sponsor/sponsor.jsx
+++ src/Components/Sponsor/sponsor.jsx
@@ -6,26 +6,26 @@ import { MdMailOutline } from "react-icons/md";
 
 const Sponsor = () => {
   return (
-    <div className="sponsor-us">
+    <div className="sponsor">
     <div className="overlay">
       <h1>Sponsor Us</h1> 
         <p>If you want to sponsor our teenagers and help provide the tools they need to succeed,
             click on the social icons below to connect with our team and learn more...
         </p>   
         <div className='sponsor-icons-container'>
-        <span className='sponsor-icons'>
-            <a href="https://wa.me/2349032628603" target="_blank" rel="noopener noreferrer">
-                <FaWhatsapp />
-            </a>
-        </span>
-        <span className='sponsor-icons'>
-            <a href="https://www.instagram.com/zedtribeafrica" target="_blank" rel="noopener noreferrer">
-                <FaInstagram />
-            </a>
-        </span>
-        <span className='sponsor-icons'>
-            <a href="mailto: zedtribeafrica@gmail.com"><MdMailOutline /></a>
-        </span>
+          <span className='sponsor-icons'>
+              <a href="https://wa.me/2349032628603" target="_blank" rel="noopener noreferrer">
+                  <FaWhatsapp />
+              </a>
+          </span>
+          <span className='sponsor-icons'>
+              <a href="https://www.instagram.com/zedtribeafrica" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram />
+              </a>
+          </span>
+          <span className='sponsor-icons'>
+              <a href="mailto: zedtribeafrica@gmail.com"><MdMailOutline /></a>
+          </span>
       </div>
     </div>
   </div>
