5f417939209f20b8281d41e6704b195f7e2ec54b
Ayotomiwa Onifara <oyindml1@gmail.com>
2024-10-25 15:40:20 +0100

    Adjusted the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa6']  // Externalize the package to avoid bundling issues
-    }
-  }
+  base: './'
 })
